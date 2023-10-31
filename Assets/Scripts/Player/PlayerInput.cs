39a52789601b91825e11130de2fe4a407fce023e
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-10-31 11:26:05 -0700

    Initial commit

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private GameObject playerShip;
+    [SerializeField] private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerShip.transform.position = transform.position + Vector3.left * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            playerShip.transform.position = transform.position + Vector3.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            playerShip.transform.position = transform.position + Vector3.down * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            playerShip.transform.position = transform.position + Vector3.up * moveSpeed;
+        }
+    }
+}
