5c6b968198e1bd54d5664e1906929a699ba1b0dd
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-11-22 10:44:01 -0800

    Nuke animation added

--- Assets/Scripts/Player/Powerups/Bomb.cs
+++ Assets/Scripts/Player/Powerups/Bomb.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-
+    [SerializeField] private List<Animator> animations = new List<Animator>();
     public void ActivateBomb()
     {
+        foreach (var anim in animations)
+        {
+            anim.SetTrigger("PlayTrigger");
+        }
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject hostiles in enemies)
             Destroy(hostiles.gameObject);
