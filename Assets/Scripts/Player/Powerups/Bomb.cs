91ea2790e80e1eeb552803f2c9e944b6bc600b98
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-11-22 11:47:49 -0800

    Nuke Effects Added

--- Assets/Scripts/Player/Powerups/Bomb.cs
+++ Assets/Scripts/Player/Powerups/Bomb.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private List<Animator> animations = new List<Animator>();
+    [SerializeField] private GameObject Nuke;
+    [SerializeField] private float DetTime;
     public void ActivateBomb()
     {
         foreach (var anim in animations)
         {
             anim.SetTrigger("PlayTrigger");
         }
+        Invoke("DetonateNuke", DetTime);
+    }
+
+    private void DetonateNuke()
+    {
+        Instantiate(Nuke, transform.position, Quaternion.identity);
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject hostiles in enemies)
+        {
             Destroy(hostiles.gameObject);
+        }
     }
 }
